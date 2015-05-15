cc6f71dc13ecda5ce7cfc9c401c19aea1a9a0a4a
d5a9p6s7 <dpoveda@gmail.com>
2015-05-15 17:39:47 -0500

    SIPT * init data sql

--- db/init-data.sql
+++ db/init-data.sql
@@ -8,6 +8,29 @@
 --
 
 -------------------------------------------------------------
--- **** - [Feature]
+-- **** - [lista]
 -------------------------------------------------------------
-
+INSERT INTO lista VALUES (1, 'arl', '');
+INSERT INTO lista VALUES (2, 'banco', '');
+INSERT INTO lista VALUES (3, 'cajacompensacion', '');
+INSERT INTO lista VALUES (4, 'cargo', '');
+INSERT INTO lista VALUES (5, 'centromedico', '');
+INSERT INTO lista VALUES (6, 'centrotrabajo', '');
+INSERT INTO lista VALUES (7, 'departamento', '');
+INSERT INTO lista VALUES (8, 'eps', '');
+INSERT INTO lista VALUES (9, 'estadovivil', '');
+INSERT INTO lista VALUES (10, 'fondocesantias', '');
+INSERT INTO lista VALUES (11, 'fondopensiones', '');
+INSERT INTO lista VALUES (12, 'genero', '');
+INSERT INTO lista VALUES (13, 'macroproyecto', '');
+INSERT INTO lista VALUES (14, 'municipio', '');
+INSERT INTO lista VALUES (15, 'parentesco', '');
+INSERT INTO lista VALUES (16, 'rh', '');
+INSERT INTO lista VALUES (17, 'sino', '');
+INSERT INTO lista VALUES (18, 'tipocontrato', '');
+INSERT INTO lista VALUES (19, 'tipocuenta', '');
+INSERT INTO lista VALUES (20, 'tipodotacion', '');
+INSERT INTO lista VALUES (21, 'tipoempresa', '');
+INSERT INTO lista VALUES (22, 'tipoformacion', '');
+INSERT INTO lista VALUES (23, 'tipoidentificacion', '');
+INSERT INTO lista VALUES (24, 'tiposoporte', '');
